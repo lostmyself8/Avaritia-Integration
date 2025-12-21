561f99c21782a2a5e7ba6acf8688715ac67193e3
CreepingCreeper <120696753+CreepingCreeper@users.noreply.github.com>
2025-12-21 14:38:19 +0800

    Fix Fluid translation key wrong

--- src/main/java/committee/nova/mods/avaritia_integration/module/tconstruct/registry/TicIntegrationFluids.java
+++ src/main/java/committee/nova/mods/avaritia_integration/module/tconstruct/registry/TicIntegrationFluids.java
@@ -18,26 +18,25 @@ import static slimeknights.tconstruct.fluids.block.BurningLiquidBlock.createBurn
 
 @Mod.EventBusSubscriber(modid = AvaritiaIntegration.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TicIntegrationFluids extends TicRegistry{
-    public static final FlowingFluidObject<ForgeFlowingFluid> molten_blaze  = FLUIDS.registerGem("molten_blaze").type(common().temperature(3300).lightLevel(15)).block(createBurning(MapColor.COLOR_RED,15, 10, 15f)).bucket().flowing();
-    public static final FlowingFluidObject<ForgeFlowingFluid> molten_crystal_matrix  = FLUIDS.registerMetal("molten_crystal_matrix").type(common().temperature(2800).lightLevel(15)).block(createBurning(MapColor.COLOR_LIGHT_BLUE,15, 10, 10f)).bucket().flowing();
-    public static final FlowingFluidObject<ForgeFlowingFluid> molten_neutron  = FLUIDS.registerMetal("molten_neutron").type(common().temperature(8300).lightLevel(15)).block(createBurning(MapColor.COLOR_BLACK,3, 10, 27f)).bucket().flowing();
-    public static final FlowingFluidObject<ForgeFlowingFluid> molten_star  = FLUIDS.registerGem("molten_star").type(common().temperature(12300).lightLevel(15)).block(createBurning(MapColor.COLOR_BLUE,15, 10, 45f)).bucket().flowing();
-    public static final FlowingFluidObject<InvertedFluid> molten_infinity  = FLUIDS.registerMetal("molten_infinity").invertedType(gas().temperature(12300).lightLevel(15)).burningBlock(MapColor.COLOR_LIGHT_GRAY,15, 10, 45f).bucket().invertedFlowing();
-    private static FluidType.Properties common() {
-        return FluidType.Properties.create().density(2000).viscosity(10000)
-                .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
-                .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
-                .motionScale(0.0023333333333333335D)
-                .canSwim(false).canDrown(false)
-                .pathType(BlockPathTypes.LAVA).adjacentPathType(null);
+    public static final FlowingFluidObject<ForgeFlowingFluid> molten_blaze  = FLUIDS.registerGem("molten_blaze").type(common("molten_blaze").temperature(3300).lightLevel(15)).block(createBurning(MapColor.COLOR_RED,15, 10, 15f)).bucket().flowing();
+    public static final FlowingFluidObject<ForgeFlowingFluid> molten_crystal_matrix  = FLUIDS.registerMetal("molten_crystal_matrix").type(common("molten_crystal_matrix").temperature(2800).lightLevel(15)).block(createBurning(MapColor.COLOR_LIGHT_BLUE,15, 10, 10f)).bucket().flowing();
+    public static final FlowingFluidObject<ForgeFlowingFluid> molten_neutron  = FLUIDS.registerMetal("molten_neutron").type(common("molten_neutron").temperature(8300).lightLevel(15)).block(createBurning(MapColor.COLOR_BLACK,3, 10, 27f)).bucket().flowing();
+    public static final FlowingFluidObject<ForgeFlowingFluid> molten_star  = FLUIDS.registerGem("molten_star").type(common("molten_star").temperature(12300).lightLevel(15)).block(createBurning(MapColor.COLOR_BLUE,15, 10, 45f)).bucket().flowing();
+    public static final FlowingFluidObject<InvertedFluid> molten_infinity  = FLUIDS.registerMetal("molten_infinity").invertedType(gas("molten_infinity").temperature(12300).lightLevel(15)).burningBlock(MapColor.COLOR_LIGHT_GRAY,15, 10, 45f).bucket().invertedFlowing();
+    private static FluidType.Properties common(String name) {
+        return base(name).density(2000);
     }
-    private static FluidType.Properties gas() {
-        return FluidType.Properties.create().density(-2000).viscosity(10000)
+    private static FluidType.Properties gas(String name) {
+        return base(name).density(-2000);
+    }
+    private static FluidType.Properties base(String name) {
+        return FluidType.Properties.create().viscosity(10000)
+                .descriptionId("fluid"+ "." + AvaritiaIntegration.MOD_ID + "." + name)
                 .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
                 .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
-                .motionScale(0.0023333333333333335D)
                 .canSwim(false).canDrown(false)
-                .pathType(BlockPathTypes.LAVA).adjacentPathType(null);
+                .pathType(BlockPathTypes.LAVA).adjacentPathType(null)
+                .motionScale(0.0023333333333333335D);
     }
     @SubscribeEvent
     public static void addToVanillaTab(BuildCreativeModeTabContentsEvent event) {
@@ -50,3 +49,4 @@ public class TicIntegrationFluids extends TicRegistry{
         }
     }
 }
+
