bc375f32e2f20919ca6deb6272d42a5186c4212b
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-04-18 23:17:23 +0800

    backport #29

--- src/main/java/committee/nova/mods/avaritia_integration/module/industrialforegoing/registry/IndustrialForegoingIntegrationItems.java
+++ src/main/java/committee/nova/mods/avaritia_integration/module/industrialforegoing/registry/IndustrialForegoingIntegrationItems.java
@@ -6,7 +6,6 @@ import committee.nova.mods.avaritia_integration.AvaritiaIntegration;
 import committee.nova.mods.avaritia_integration.init.registry.AIItems;
 import committee.nova.mods.avaritia_integration.module.industrialforegoing.item.AddonInfo;
 import committee.nova.mods.avaritia_integration.module.industrialforegoing.item.AddonItem;
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,7 +21,7 @@ public class IndustrialForegoingIntegrationItems {
 
     private static HashMap<String, RegistryObject<AddonItem>> registryAddons(){
         HashMap<String, RegistryObject<AddonItem>> map = new HashMap<>();
-        AddonInfo.create(3,AIItems.BLAZE_CUBE_GEAR, ModuleCore.ETHER.getSourceFluid(),1000,100,"blaze_cube").registry(map,ITEMS);
+        AddonInfo.create(3,AIItems.BLAZE_CUBE_GEAR, () -> ModuleCore.ETHER.getSourceFluid().get(),1000,100,"blaze_cube").registry(map,ITEMS);
         AddonInfo.create(5,AIItems.CRYSTAL_MATRIX_GEAR,IndustrialForegoingIntegrationFluids.ELDERLY_MEDULLA.getSourceFluid(),1000,200,"crystal_matrix").registry(map,ITEMS);
         AddonInfo.create(8,ModItems.neutron_gear,IndustrialForegoingIntegrationFluids.VOID_MATTER.getSourceFluid(),1000,300,"neutron").registry(map,ITEMS);
         AddonInfo.create(12,AIItems.INFINITY_GEAR,IndustrialForegoingIntegrationFluids.VOID_MATTER.getSourceFluid(),2000,400,"infinity").registry(map,ITEMS);
